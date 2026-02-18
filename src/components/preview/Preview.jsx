1494fd773da0ae95f87d86471dbec4c958a5b4b5
ZakirMagide <62655140+Zaky-dc@users.noreply.github.com>
2026-02-18 13:39:03 +0200

    Refactor Preview component to remove forwardRef
    Removed forwardRef and simplified the Preview component.

--- src/components/preview/Preview.jsx
+++ src/components/preview/Preview.jsx
@@ -1,11 +1,9 @@
-import { forwardRef } from "react";
 import { useResume } from "../../context/ResumeContext";
 import Modern from "../templates/Modern";
 import Classic from "../templates/Classic";
 import Creative from "../templates/Creative";
 
-// Usamos forwardRef para que o MainLayout consiga "agarrar" o elemento
-const Preview = forwardRef((props, ref) => {
+const Preview = () => {
     const { resumeData } = useResume();
     const { templateId } = resumeData.metadata;
 
@@ -18,19 +16,10 @@ const Preview = forwardRef((props, ref) => {
     const TemplateComponent = templates[templateId] || Modern;
 
     return (
-        /* IMPORTANTE: 
-           1. Adicionamos 'bg-white' e 'text-black' para anular qualquer Dark Mode do sistema.
-           2. REMOVEMOS 'h-full' ou 'h-screen'. Deixamos a altura ser automática.
-        */
-        <div 
-            ref={ref} 
-            className="w-full bg-white text-black min-h-[297mm]"
-        >
+        <div className="w-full bg-white text-black">
             <TemplateComponent data={resumeData} />
         </div>
     );
-});
-
-Preview.displayName = "Preview";
+};
 
 export default Preview;
